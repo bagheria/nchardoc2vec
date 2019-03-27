b935ba6113c1a826084f6768bce3e2c29ee7d392
Ayoub Bagheri <ayoub.bagheri@gmail.com>
2019-03-27 11:58:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,16 @@ import warnings
 warnings.filterwarnings("ignore")
 from doc2vec_gensim import Doc2Vec, TaggedDocument
 
+
 logging.basicConfig(
     format='%(asctime)s : %(levelname)s : %(message)s',
     level=logging.INFO)
+
+
+def vector_for_learning(model, tagged_docs):
+    regressors = [(model.infer_vector(doc.words, steps=20)) for doc in tagged_docs]
+    return regressors
+
 
 if __name__ == '__main__':
     data_file = "D:/Data/ohsumed-first-20000-docs/training/"
@@ -19,6 +26,7 @@ if __name__ == '__main__':
     # each text item becomes a series of words
     # so this becomes a list of lists
     t = cgrams2vec.cgrams2vec(data_file, num_epochs=2)
+
     # t.create_dataset()
     # t.train_embedding()
     # y_train = word2vec_medic.read_target(data_file)
@@ -33,6 +41,7 @@ if __name__ == '__main__':
         window=10,
         min_count=2,
         workers=10)
+
     # Default algo in Gensim is CBOW. It is used if you simply call gensim.models.Word2Vec(sentences)
     # In order to use skipgram run gensim.models.Word2Vec(sentences, sg=1)
     model.train(t.documents, total_examples=len(t.documents), epochs=10)
@@ -73,9 +82,21 @@ if __name__ == '__main__':
                                                               max_iter=100,
                                                               multi_class='multinomial'))])
 
-    # doc2vec model from gensim
-    tagged_data = [TaggedDocument(words=t.documents[i], tags=[str(i)]) for i, _d in enumerate(t.documents)]
-    max_epochs = 5 # 100
+    # run all the models with logistic regression:
+    all_models = [
+        ("LR_w2v_mean", LR_w2v_mean),
+        ("LR_w2v_tfidf", LR_w2v_tfidf),
+        ("LR_c2v_mean", LR_c2v_mean),
+        ("LR_c2v_tfidf", LR_c2v_tfidf),
+    ]
+
+    for name, model in all_models:
+        score = cross_val_score(model, t.documents, t.y_train, cv=5).mean()
+        print(name, score)
+
+    # doc2vec model
+    tagged_data_train = [TaggedDocument(words=t.documents[i], tags=[str(i)]) for i, _d in enumerate(t.documents)]
+    max_epochs = 100
     vec_size = 20
     alpha = 0.025
     d_model = Doc2Vec(size=vec_size,
@@ -83,10 +104,10 @@ if __name__ == '__main__':
                     min_alpha=0.00025,
                     min_count=1,
                     dm=1)
-    d_model.build_vocab(tagged_data)
+    d_model.build_vocab(tagged_data_train)
     for epoch in range(max_epochs):
         print('iteration {0}'.format(epoch))
-        d_model.train(tagged_data,
+        d_model.train(tagged_data_train,
                     total_examples=d_model.corpus_count,
                     epochs=d_model.iter)
         # decrease the learning rate
@@ -94,21 +115,6 @@ if __name__ == '__main__':
         # fix the learning rate, no decay
         d_model.min_alpha = d_model.alpha
 
-    LR_doc2vec = Pipeline([("doc2vec vectorizer", d_model.docvecs),
-                                  ("LR", LogisticRegression(random_state=0,
-                                                            solver='lbfgs',
-                                                            max_iter=100,
-                                                            multi_class='multinomial'))])
-
-    # run all the models with logistic regression:
-    all_models = [
-        ("LR_w2v_mean", LR_w2v_mean),
-        ("LR_w2v_tfidf", LR_w2v_tfidf),
-        ("LR_c2v_mean", LR_c2v_mean),
-        ("LR_c2v_tfidf", LR_c2v_tfidf),
-        ("LR_doc2vec", LR_doc2vec)
-    ]
-
-    for name, model in all_models:
-        score = cross_val_score(model, t.documents, t.y_train, cv=5).mean()
-        print(name, score)
+    X_train_doc2vec = vector_for_learning(d_model, tagged_data_train)
+    logreg = LogisticRegression(n_jobs=1, C=1e5)
+    print("LR_doc2vec ", cross_val_score(logreg, X_train_doc2vec, t.y_train, cv=5).mean())
